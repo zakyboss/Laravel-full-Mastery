ae9245f7abc89f9fac85f6847bc2aed6aba2a896
Zakyboss <Boochimo8@gmail.com>
2025-05-17 22:13:50 +0300

    Using wild card

--- example/resources/views/home.blade.php
+++ example/resources/views/home.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 
     <x-slot:heading> <h1>This is Home heading</h1></x-slot:heading>
-        <h1> Hello Zakyboss ! From laravel Baby !</h1>
+        <h1> {{$greet}} Zakyboss ! From laravel Baby !</h1>
             
 </x-layout>
