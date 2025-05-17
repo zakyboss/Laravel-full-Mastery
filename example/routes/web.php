aff5124c28f08d0dd7ce15f2513fc75ce54eb489
Zakyboss <Boochimo8@gmail.com>
2025-05-17 22:12:27 +0300

    Creating jobs route

--- example/routes/web.php
+++ example/routes/web.php
@@ -3,11 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',['greet'=> "yello"]);
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs',['jobs'=> 
+
+    [
+        ['Description'=> 'Programmer' , 'Salary'=> 30000, 'YOE'=> 3],
+        ['Description'=> 'Doctor' , 'Salary'=> 900000, 'YOE'=> 2],
+        ['Description'=> 'Lawyer' , 'Salary'=> 340000, 'YOE'=> 1],
+        ['Description'=> 'Captain' , 'Salary'=> 2320000, 'YOE'=> 13],
+]]);
 });
 
 Route::get('/contacts', function (){
